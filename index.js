3f81b4f4ec04a044c729d2e7b982c9b7c8d337d2
John Eswin Nizar <jenizar@users.noreply.github.com>
2020-02-25 21:22:14 +0700

    Twitter Sentiment Analysis
    Deploy NodeJS apps ** Twitter Sentiment Analysis ** on SAP Cloud Platform using Cloud Foundry

---
+++ index.js
@@ -0,0 +1,59 @@
+const Twit = require('twit');
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const path = require('path');
+
+// const { execSync } = require('child_process');
+// // stderr is sent to stderr of parent process
+// // you can set options.stdio if you want it to go elsewhere
+// console.log('la ruta');
+// console.log(execSync('ls').toString('utf8'));
+
+const T = new Twit({
+  consumer_key:         'MWQadaodeKZ02FPTfkJA8OhF9',
+  consumer_secret:      'hVGVTiEtAEFwzLZLB6gpa8IaI2xrdv7l4G7S61QyvI3lrPwcU3',
+  access_token:         '2942702198-rybhZK6kteq3c1KyIoAAMtV3C49Q8LlU2VkQcz5',
+  access_token_secret:  'ErFaebAlbAfL1e4yh2NjEx1VDFdQq8En6OurUbHYyUKKY',
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+});
+
+http://expressjs.com/en/starter/static-files.html   
+app.use(express.static('Public'));
+
+const brand = '@BBCNews,@Reuters,@business,@AP'; 
+
+io.on('connection', function(socket) {
+  
+  T.get('search/tweets', { q: brand, count: 100 }, function(err, data, response) {
+    var tweetArray=[];
+    for (let index = 0; index < data.statuses.length; index++) {
+            const tweet = data.statuses[index];
+            var tweetbody = {
+              'text': tweet.text,
+              'userScreenName': "@" + tweet.user.screen_name,
+              'userImage': tweet.user.profile_image_url_https,
+              'userDescription': tweet.user.description,
+            }
+            tweetArray.push(tweetbody);
+        }     
+        io.emit('allTweet',tweetArray)
+    })
+
+    var stream = T.stream('statuses/filter', { track: brand, language: 'en', filter_level: 'none' })
+
+    stream.on('tweet', function (tweet) {
+        console.log(tweet);
+        io.emit('tweet',{ 'tweet': tweet });
+    })
+});
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'Public', 'index.html'));
+});
+
+// listen for requests :)
+const listener = server.listen(process.env.PORT || 3000, function() {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
